373e4fb21e6dbf5a7ca14a25b5c8f3be33ae8d5a
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-08-15 13:26:50 +0200

    added scoreboard

--- logic.js
+++ logic.js
@@ -4,76 +4,98 @@ const ROCK_WINS = "Rock beats Scissors";
 const PAPER_WINS = "Paper beats Rock";
 const SCISSORS_WINS = "Scissors beats Paper";
 const resultDiv = document.querySelector('.result');
+const btns = Array.from(document.querySelectorAll('button'));
 
 // EFFECTS: returns a random selection between the 3 options of Rock, Paper, and Scissors.
 function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
     let choice = Math.floor(Math.random() * options.length);
-    console.log(options[choice]);
     return options[choice];
 }
 
 // REQUIRES: two strings that represent the Player's selection, Computer's selection
-// EFFECTS: produces the result of a round of the game with the given selections
+// EFFECTS: produces true if user wins game, false if computer wins, and null if there's a tie.
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     computerSelection = computerSelection.toLowerCase()
     if (playerSelection === computerSelection) {
         resultDiv.innerHTML = "It's a tie!";
+        return null;
     }
 
     selectionPair = playerSelection + computerSelection;
-    console.log(selectionPair);
 
     switch (selectionPair) {
         case "rockpaper":
             resultDiv.innerHTML = LOSE + PAPER_WINS;
-            return LOSE + PAPER_WINS;
+            return false;
         
         case "rockscissors":
             resultDiv.innerHTML = WIN + ROCK_WINS;
-            return WIN + ROCK_WINS;
+            return true;
 
         case "paperrock":
             resultDiv.innerHTML = WIN + PAPER_WINS;
-            return WIN + PAPER_WINS;
+            return true;
         
         case "paperscissors":
             resultDiv.innerHTML = LOSE + SCISSORS_WINS;
-            return LOSE + SCISSORS_WINS;
+            return false;
 
         case "scissorsrock":
             resultDiv.innerHTML = LOSE + ROCK_WINS;
-            return LOSE + ROCK_WINS;
+            return false;
 
         default:
             resultDiv.innerHTML = WIN + SCISSORS_WINS;
-            return WIN + SCISSORS_WINS;
+            return true;
     }
 
+}
+
+function updateScoreboard(player, computer) {
+    let scoreBoard = document.querySelector('.score');
+    scoreBoard.textContent = `Score (P - C): ${player} - ${computer}`;
+}
+
+function displayEndMessage(player, computer) {
+    // Positive score means player is winning
+    // Negatice score means computer is winning
+    btns.forEach(btn => btn.removeEventListener('click', playRound));
+    resultDiv.remove();
+    if (player > computer) {
+        return `Congrats, you are the Winner by ${player - computer} point/s`;
+    } else if (player < computer) {
+        return `Oh No! You lost by ${computer - player} point/s`;
+    } else {
+        return `It was a tie!`;
+    }
 }
 
 // EFFECTS: runs the game for 5 rounds, keeps score and reports a winner
 function game() {
-    let userChoice;
-    let score = 0;
-
-    // Positive score means player is winning
-    // Negatice score means computer is winning
-    if (score > 0) {
-        return `Congrats, you are the Winner by ${score} point/s`
-    } else if (score < 0) {
-        return `Oh No! You lost by ${score * -1} point/s`
-    } else {
-        return `It was a tie!`
-    }
-}
-
-const btns = Array.from(document.querySelectorAll('button'));
-    console.log(btns);
+    let player = 0;
+    let computer = 0;
+    let round = 0;
     btns.forEach(btn => btn.addEventListener('click', function () {
-        userChoice = btn.getAttribute('id');
-        console.log(userChoice);
-        playRound(userChoice, getComputerChoice());
+        let userChoice = btn.getAttribute('id');
+        let roundResult = playRound(userChoice, getComputerChoice());
+
+        if (roundResult === true) {
+            player++;
+        } else if (roundResult === false) {
+            computer++;
+        } 
+
+        if (round === 5) {
+            displayEndMessage(player, computer);
+            return;
+        }
+        updateScoreboard(player, computer);
+        round++;
+        console.log(round);
     }));
 
+}
+
+game();
