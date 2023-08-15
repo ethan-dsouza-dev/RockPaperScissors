e3db07f1357033bd63886265a0a22115988d318d
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-08-15 10:49:58 +0200

    fixed wrong result issue, fully working now

--- logic.js
+++ logic.js
@@ -1,47 +1,53 @@
 const WIN = "You Win! ";
 const LOSE = "You Lose! ";
-
+const ROCK_WINS = "Rock beats Scissors";
+const PAPER_WINS = "Paper beats Rock";
+const SCISSORS_WINS = "Scissors beats Paper";
+const resultDiv = document.querySelector('.result');
 
 // EFFECTS: returns a random selection between the 3 options of Rock, Paper, and Scissors.
 function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
     let choice = Math.floor(Math.random() * options.length);
-
+    console.log(options[choice]);
     return options[choice];
 }
 
 // REQUIRES: two strings that represent the Player's selection, Computer's selection
 // EFFECTS: produces the result of a round of the game with the given selections
 function playRound(playerSelection, computerSelection) {
-    const ROCK_WINS = "Rock beats Scissors";
-    const PAPER_WINS = "Paper beats Rock";
-    const SCISSORS_WINS = "Scissors beats Paper";
-
     playerSelection = playerSelection.toLowerCase()
     computerSelection = computerSelection.toLowerCase()
     if (playerSelection === computerSelection) {
-        return "It's a tie!"
+        resultDiv.innerHTML = "It's a tie!";
     }
 
     selectionPair = playerSelection + computerSelection;
+    console.log(selectionPair);
 
     switch (selectionPair) {
         case "rockpaper":
+            resultDiv.innerHTML = LOSE + PAPER_WINS;
             return LOSE + PAPER_WINS;
         
         case "rockscissors":
+            resultDiv.innerHTML = WIN + ROCK_WINS;
             return WIN + ROCK_WINS;
 
         case "paperrock":
+            resultDiv.innerHTML = WIN + PAPER_WINS;
             return WIN + PAPER_WINS;
         
         case "paperscissors":
+            resultDiv.innerHTML = LOSE + SCISSORS_WINS;
             return LOSE + SCISSORS_WINS;
 
         case "scissorsrock":
+            resultDiv.innerHTML = LOSE + ROCK_WINS;
             return LOSE + ROCK_WINS;
 
-        case "scissorspaper":
+        default:
+            resultDiv.innerHTML = WIN + SCISSORS_WINS;
             return WIN + SCISSORS_WINS;
     }
 
@@ -51,13 +57,6 @@ function playRound(playerSelection, computerSelection) {
 function game() {
     let userChoice;
     let score = 0;
-
-    const btns = Array.from(document.querySelectorAll('button'));
-    console.log(btns);
-    btns.forEach(btn => btn.addEventListener('click', function () {
-        userChoice = btn.getAttribute('id');
-        playRound(userChoice, getComputerChoice());
-    }));
 
     // Positive score means player is winning
     // Negatice score means computer is winning
@@ -70,5 +69,11 @@ function game() {
     }
 }
 
-game();
+const btns = Array.from(document.querySelectorAll('button'));
+    console.log(btns);
+    btns.forEach(btn => btn.addEventListener('click', function () {
+        userChoice = btn.getAttribute('id');
+        console.log(userChoice);
+        playRound(userChoice, getComputerChoice());
+    }));
 
